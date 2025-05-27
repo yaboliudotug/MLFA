1116c06b2b79485ae7e34822fc8f7cf0c3a8f7e0
yaboliudotug <yaboliu.ug@gmail.com>
2025-05-27 23:19:38 +0800

    codes

--- test_time_adaptation.sh
+++ test_time_adaptation.sh
@@ -1,10 +1,10 @@
 ## TTA
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/city_to_foggy_vgg16_ok.yaml 
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/kitti_to_city_vgg16_ok.yaml
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/sim10k_to_city_vgg16_ok.yaml
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/city_to_foggy_vgg16.yaml 
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/kitti_to_city_vgg16.yaml
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/sim10k_to_city_vgg16.yaml
 
 
 ## source only
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/city_to_foggy_vgg16_ok.yaml --test_only
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/kitti_to_city_vgg16_ok.yaml --test_only
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/sim10k_to_city_vgg16_ok.yaml --test_only
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/city_to_foggy_vgg16.yaml --test_only
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/kitti_to_city_vgg16.yaml --test_only
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py --config-file configs/sim10k_to_city_vgg16.yaml --test_only
